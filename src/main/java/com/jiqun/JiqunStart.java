a26f001d6ff68bc0d62a0b39f815b38e32be69e2
liuzhenzhen <315475165@qq.com>
2020-07-14 21:17:32 +0800

    提交

---
+++ src/main/java/com/jiqun/JiqunStart.java
@@ -0,0 +1,11 @@
+package com.jiqun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiqunStart {
+    public static void main(String[] args) {
+        SpringApplication.run(JiqunStart.class);
+    }
+}
